e71455038724a86b877a2d87f40353631b562ad2
KaiserX8 <a9244218@rn.senac.br>
2019-07-25 20:56:35 -0300

    adições

--- js/cadastro.js
+++ js/cadastro.js
@@ -1,5 +1,7 @@
 function cadastrar() {
 
+    let id = document.getElementById('id').value;
+
     let nome = validaNome(document.getElementById('nome').value);
     if(nome == false) {
         alert('Insira um nome válido!');
@@ -20,14 +22,14 @@ function cadastrar() {
         sexo = 'Prefiro não comentar';
     }
     
-    inserirNaTabela(nome, fone, sexo, cidade);
+    inserirNaTabela(id, nome, fone, sexo, cidade);
 
     limparFormulario();
 
 }
 
 function limparFormulario() {
-    document.getElementById('nome').value = '';
+    document.getElementById('nome').value = 0;
     document.getElementById('fone').value = '';
     document.getElementById('cidade').value = 'Natal';
     document.getElementById('masc').checked = true;
@@ -48,28 +50,36 @@ function ehNumero(numero) {
     return !isNaN(numero)
 }
 
-function teste() {
+function insereNaTabela(id, nome, fone, sexo, cidade) {
     let tabela = document
         .getElementById("lista-contatos")
         .getElementByTagName('tbody')[0];
 
     let ultimaLinha = tabela.rows.length;
-
+    
+    if (id == 0) {
     let linha  = tabela.insertRow(ultimaLinha);
-    let id = linha.insertCell(0);
-    let nome = linha.insertCell(1);
-    let fone = linha.insertCell(2);
-    let sexo = linha.insertCell(3);
-    let cidade = linha.insertCell(4);
-    let acoes ;
+    
+    let campoId = linha.insertCell(0);
+    let campoNome = linha.insertCell(1);
+    let campoFone = linha.insertCell(2);
+    let campoSexo = linha.insertCell(3);
+    let campoCidade = linha.insertCell(4);
+    let acoes = linha.insertCell(5);
 
-    id.innnerHTML = ultimaLinha;
+    id.innnerHTML = ultimaLinha + 1;
     nome.innnerHTML = nome;
     fone.innnerHTML = fone;
     sexo.innnerHTML = sexo;
     cidade.innnerHTML = cidade;
-    acoes.innnerHTML = document.write(insereBotoesAcoes());
+    acoes.innnerHTML = insereBotoesAcoes(ultimaLinha + 1);
     
+} else {
+    let linha = id - 1;
+    tabela.rows[linha].cells[1].innnerHTML = nome;
+    tabela.rows[linha].cells[2].innnerHTML = fone;
+    tabela.rows[linha].cells[3].innnerHTML = sexo;
+    tabela.rows[linha].cells[4].innnerHTML = cidade;
 }
 
 function insereBotoesAcoes() {
@@ -90,9 +100,24 @@ function buscaContatoPeloId(id) {
         .getElementByTagName('tbody')[0];
     let qtdLinhas = body.rows.length;
     for (let i = 0; i < qtdLinhas; i++) {
-        if (body.rows[i].cells[1].innnerHTML == id) {
+        if (body.rows[i].cells[0].innnerHTML == id) {
             let inputNome = document.getElementById('nome');
             inputNome.value = body.rows[i].cells[1].innnerHTML;
+            
+            let inputFone = document.getElementById('fone');
+            inputFone.value = body.rows[i].cells[2].innnerHTML;
+            
+            let sexo = body.rows[i].cells[3].innnerHTML.toLowerCase();
+            if (sexo == 'masculino') {
+                document.getElementById('masc').checked = true;
+            } else if (sexo == 'feminino') {
+                document.getElementById('fem').checked = true;
+            }else {
+                document.getElementById('indef').checked = true;
+            }
+            let selectCidade = document.getElementById('cidade');
+            selectCidade.value = body.rows[i].cells[4].innnerHTML;
+
             return;
         }
     }
